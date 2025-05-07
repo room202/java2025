a53aeb21dccdde65170ea76a492ef39aeaa5807c
room202 <yagi@i-seifu.jp>
2025-05-07 10:43:13 +0900

    練習1-3の模範解答追加

---
+++ j0507/Main1_3.java
@@ -0,0 +1,22 @@
+package j0507;
+
+// P.060 練習1-3
+public class Main1_3 {
+    public static void main(String[] args) {
+        // true
+        boolean result = true;
+        System.out.println(result);
+        // '駆'
+        char moji = '駆';
+        System.out.println(moji);
+        // 3.14
+        double pi = 3.14;
+        System.out.println(pi);
+        // 314159265853979L
+        long num = 314159265853979L;
+        System.out.println(num);
+        // "ミナトの攻撃！敵に15ポイントのダメージを与えた。"
+        String msg = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+        System.out.println(msg);
+    }
+}
